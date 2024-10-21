70c1c8810f78f6ffb325dd8770e327fd60b5ac12
LorenzoMiechielsen <r0885689@student.thomasmore.be>
2024-10-21 15:21:46 +0200

    first commit of bezoeker-service

---
+++ bezoeker-service/src/main/java/fact/it/bezoekerservice/BezoekerServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.bezoekerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezoekerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BezoekerServiceApplication.class, args);
+    }
+
+}
---
+++ bezoeker-service/src/test/java/fact/it/bezoekerservice/BezoekerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.bezoekerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezoekerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
