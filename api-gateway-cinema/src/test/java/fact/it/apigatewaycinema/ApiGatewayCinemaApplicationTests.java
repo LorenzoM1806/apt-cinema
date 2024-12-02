0c8ad7db6dfa8cd0b0a3e146d903f02d9fc8143e
LorenzoMiechielsen <r0885689@student.thomasmore.be>
2024-12-02 16:37:21 +0100

    gateway

---
+++ api-gateway-cinema/src/test/java/fact/it/apigatewaycinema/ApiGatewayCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.apigatewaycinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
